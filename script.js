54f50df51281b915237b495801e3ee7a3195dfea
izaiasjr <up1.izaiasjr@gmail.com>
2022-07-04 02:25:46 -0300

    meu desafio 01 Js de Ingles

---
+++ script.js
@@ -0,0 +1,49 @@
+//Crie 3 variáveis, cada uma com um array:
+
+let comedy = [" As Branquelas"]
+console.log (comedy)
+
+let Action = ["Django Livre"]
+console.log (Action)
+
+let Fantasy = ["Doutor Estranho no Multiverso da Loucura"]
+console.log (Fantasy)
+
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+const movie = [
+    {
+        title:" As Branquelas",
+        director:["Keenen Ivory Wayans"],
+        year: 2004
+    },
+    
+    {
+        title:"Django Livre",
+        director:["Quentin Tarantino"],
+        year: 2013
+    },
+    {
+        title:"Doutor Estranho no Multiverso da Loucura",
+        director:["Sam Raimi"],
+        year: 2022
+    },
+
+];
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+
+
+
+if( comedy[0].title === 'velozes e Furiosos' && comedy[1].title === 'Minha Mãe é Uma Peça 3' &&  comedy[2].title === 'Deadpool'){
+    console.log(`MY FAVORIT MOVIE IS ${comedy[0].title}` );
+  }
+  else{console.log(`NOT EVEN 5 STARS`)}
+
+
+
+
+
+
+
+
+
